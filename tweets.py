2323aee21362a35b3e08ca66b8e75a3cbfb89a69
Jin Moon <jym2584@rit.edu>
2021-02-20 17:11:26 -0500

    Update tweets.py

--- tweets.py
+++ tweets.py
@@ -24,37 +24,44 @@ def get_tweets(user, num_tweets = 1):
                                 include_rts = False,
                                 tweet_mode = 'extended')
     i = 0
-    for info in user_tl:
+    
+    for info in user_tl:   # Need some help trying to get data off of user_tl without doing for loop
         i = i + 1
         print("Tweet #" + str(i))
         print("Tweet ID " + str(info.id))
         print("Created " + str(info.created_at))
         print("Contents: \n" + info.full_text)
-    
-    return str(info.id)
 
-def get_status(info_id, user):
+def get_new_tweet(user):
+    getuser = api.get_user(user)
+    user_tl = api.user_timeline(screen_name = getuser.screen_name,
+                                count = 1,
+                                include_rts = False,
+                                tweet_mode = 'extended')
+    
+    for info in user_tl:
+        tweet_id = str(info.id) # Getting the most recent tweet ID, might need to refactor
+                                # to not include the for loop
+
     i = 0
     with open("recent_tweet.txt", "r+") as file:
         for line in file:
-            for id_num in info_id:
+            line = str(line)
+            for id_num in tweet_id:
+                id_num = str(id_num)
                 if line[i] != id_num: # If there is a mismatch on the characters
+                    print("@" + getuser.screen_name + " has posted a new tweet") # We found the tweet
+                    
+                    for info in user_tl:                        # Might need some help
+                        print("Contents: \n" + info.full_text)
+                        print("(Posted at " + str(info.created_at) + ")")
 
-                    print("Found new tweet!") # We found the tweet
-                    new_info_id = get_tweets(user, 1) # Get the tweet
                     file.seek(0) # Go to the beginning of the text file
                     file.truncate() # Remove the contents of the text file
-                    file.write(new_info_id) # Write the new info id
+                    file.write(tweet_id) # Write the new info id
                     file.close()
-                    return new_info_id # return the new tweet id
-                print("No new tweet found.")
-                return None
+                    return 0
+                i += 1
 
-
-get_status(get_tweets("ritTigers"), "ritTigers")
-
-#redirect_url = auth.get_authorization_url()
-#webbrowser.open(redirect_url)
-#user_pin_input = input("Whats the pin value: ")
-#auth.get_access_token(user_pin_input)
-#print(auth.access_token, auth.access_token_secret)
+#get_tweets("ritTigers")
+get_new_tweet("ritTigers")
