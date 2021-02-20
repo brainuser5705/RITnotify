2fb364dcfbfe18d3aa051dde554e16599c48bd22
Ashley <acl9213@rit.edu>
2021-02-20 13:43:22 -0500

    Discord bot framework

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+intents = discord.Intents(messages=True, members=True, guilds=True, presences=True)
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='rit ')
+
