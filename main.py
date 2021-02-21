bf21e34d73a4c804926a216acade7c324d68cac7
Jin Moon <jym2584@rit.edu>
2021-02-20 21:03:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
-
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+import praw
+from datetime import datetime
 
 intents = discord.Intents(messages=True, members=True, guilds=True, presences=True)
 
@@ -18,5 +19,26 @@ async def on_ready():
 async def pingpong(ctx):
     response = 'pong'
     await ctx.send(response)
+
+@bot.command(name='reddit_post')
+async def grab_posts(ctx):
+    REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
+    REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
+    USER_AGENT = "RIT bot for hackathon"
+
+    reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
+                            client_secret=REDDIT_CLIENT_SECRET,
+                            user_agent=USER_AGENT,
+                            check_for_async=False)
+    for submission in reddit.subreddit("rit").new(limit=1):
+        if not submission.stickied:
+            embed=discord.Embed(
+            title=submission.title,
+                url=submission.url,
+                description=submission.selftext,
+                color=discord.Color.blue())
+            embed.set_footer(text= "by "+ str(submission.author) + " on " + datetime.utcfromtimestamp(int(submission.created_utc)).strftime('%Y-%m-%d %H:%M:%S'))
+            await ctx.send(embed=embed)
+
 
 bot.run(TOKEN)
