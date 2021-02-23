c35164924b9aa3ae1bcd5d5323c61d70a2e06989
Ashley <brainuser5705@protonmail.ch>
2021-02-22 23:27:25 -0500

    Revised the code for get_menu, more efficient and cleaner output

--- bot/dining.py
+++ bot/dining.py
@@ -10,54 +10,51 @@ from datetime import datetime
 MENU_URL = 'https://www.rit.edu/fa/diningservices/daily-specials'
 HOURS_URL = 'https://www.rit.edu/fa/diningservices/places-to-eat/hours'
 
-def get_menu(location_name):
+def get_menu(loc_arg):
 
     page = requests.get(MENU_URL)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    location_ids = {'bcc': "103", 'cmc': "104", 'tc': "105", 'gracies': "107"}
-    location_names = {'bcc': "Brick City Cafe", 'cmc': "Cafe & Market at Crossroads", 'tc': "The Commons",
-                      'gracies': "Gracie's"}
-    meals = {'DINNER', 'LUNCH', 'BREAKFAST'}
-    content = soup.find('div', class_='ds-output')
+    loc_ids = {'bcc': "103", 'cmc': "104", 'tc': "105", 'gracies': "107"}
 
-    id = location_ids.get(location_name)
-    menu_section = content.find(id=id)
+    menu_tag = soup.find(id=loc_ids.get(loc_arg))
+    loc_name = menu_tag.find('h3').text
 
-    s = 'Showing  menus for: ' + menu_section.find('h3').text
-    error = ""
+    menus = menu_tag.find('div', class_='ds-loc-title')
+
     embeds = []
 
-    for meal in meals:
+    if not menus.contents:
+        string = '**' + loc_name + '** is closed today.'
+    else:
+        string = 'Showing  menus for: **' + loc_name + '**'
 
-        embed = discord.Embed(title=meal, colour=Colour.orange())
+        for menu in menus.findChildren(recursive=False): #only find direct children, the menu <BREAKFAST-107>
 
-        meal_menu = menu_section.find(id=meal + '-' + id)
-        if meal_menu is None:
-            error = "**" + location_names.get(location_name) + ' is closed today.**'
-            break
+            menu_name = menu.find('div', class_='menu-type').text
 
-        items_section = meal_menu.find('div', class_='col-xs-12 col-md-12 menu-category-items')
-        if items_section is None:
-            embed.add_field(name='No ' + meal + ' menu today.', value='------')
-            embeds.append(embed)
-            continue
+            embed = discord.Embed(title=menu_name, color=Colour.orange())
 
-        options_list = items_section.find_all('div', class_='col-xs-12 col-md-6 menu-category-list')
-        for option in options_list:
-            station = option.find('div', class_='menu-category')
-            dish = option.find('div', class_='menu-items')
+            items_section = menu.find('div', class_='col-xs-12 col-md-12 menu-category-items')
 
-            dish_string = ""
-            for content in dish.contents:
-                if str(content) not in ['<br/>', '\n']:
-                    dish_string += '- ' + str(content) + '\n'
-                
-            embed.add_field(name='**' + station.text + '**', value=dish_string)
+            if items_section is None:
+                embed.add_field(name='No ' + menu_name + ' today.', value='-')
 
-        embeds.append(embed)
+            stations = items_section.find_all('div', class_='col-xs-12 col-md-6 menu-category-list') # b/c there is another div class='clear'
+            for item in stations:
+                station_name = item.find('div', class_='menu-category').text
+                dish = item.find('div', class_='menu-items')
 
-    return s, error, embeds
+                dish_string = ""
+                for content in dish.contents[:-1]: # there is a new line at the end
+                    if str(content) != '<br/>':
+                        dish_string += '- ' + str(content) + '\n'
+
+                embed.add_field(name=station_name, value=dish_string)
+
+            embeds.append(embed) # outside of the for loop after everything is added
+
+    return string, embeds
 
 def get_hours(is_include_closed):
     current_time = datetime.now()
